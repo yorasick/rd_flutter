f240ece9980f8d8f9f029730a4bc0166e4b4e569
Yurii Kalishchuk <yurii@workerbase.com>
2025-07-14 20:33:39 +0300

    feat(DART-11): fetch age

--- hw_11/dart_application/bin/hw_futures.dart
+++ hw_11/dart_application/bin/hw_futures.dart
@@ -1,9 +1,20 @@
+import 'dart:math';
+
 Future<String> fetchName() async {
   await Future.delayed(const Duration(seconds: 2));
   return 'Yura';
 }
 
+Future<String> fetchAge() {
+  return Future.delayed(
+    const Duration(milliseconds: 1500),
+    () => (Random().nextInt(20) + 15).toString(),
+  );
+}
+
 void main() async {
   final name = await fetchName();
   print('My name is $name');
+  final age = await fetchAge();
+  print('My age is $age');
 }
