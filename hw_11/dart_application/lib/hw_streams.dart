e175d0303189b58fc7ae5a49fbd745bd648d8ba7
Yurii Kalishchuk <yurii@workerbase.com>
2025-07-14 21:24:50 +0300

    feat(DART-11): add reverse countdown witn stream

--- hw_11/dart_application/lib/hw_streams.dart
+++ hw_11/dart_application/lib/hw_streams.dart
@@ -1,10 +1,10 @@
 Future<void> streamFromIterable() async {
+  print('----------------Stream from Iterable----------------');
   final stream = Stream.fromIterable([1, 2, 3, 4, 5]);
 
   await for (final number in stream) {
     print(number);
   }
-  print('--------------------------------');
 
   final stream2 = Stream.fromIterable([1, 2, 3, 4, 5]);
   stream2.listen(
@@ -20,6 +20,19 @@ Future<void> streamFromIterable() async {
   );
 }
 
+Future<void> reverseCountdown() async {
+  print('----------------Reverse Countdown----------------');
+  final stream = Stream.periodic(
+    const Duration(seconds: 1),
+    (i) => i + 1,
+  ).take(10);
+
+  await for (final number in stream) {
+    print('$number...');
+  }
+}
+
 Future<void> main() async {
   await streamFromIterable();
+  await reverseCountdown();
 }
