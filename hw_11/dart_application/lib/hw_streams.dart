7fc02935bed0693049c87e5c3dd1b0e226bb8a54
Yurii Kalishchuk <yurii@workerbase.com>
2025-07-14 21:28:32 +0300

    feat(DART-11): stream controller

--- hw_11/dart_application/lib/hw_streams.dart
+++ hw_11/dart_application/lib/hw_streams.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 Future<void> streamFromIterable() async {
   print('----------------Stream from Iterable----------------');
   final stream = Stream.fromIterable([1, 2, 3, 4, 5]);
@@ -32,7 +34,28 @@ Future<void> reverseCountdown() async {
   }
 }
 
+void streamController() {
+  print('----------------Stream Controller----------------');
+  final streamController = StreamController<String>();
+
+  streamController.add('Hello');
+  streamController.add('World');
+  streamController.add('Dart');
+
+  streamController.stream.listen(
+    (data) {
+      print(data);
+    },
+    onDone: () {
+      print('Stream was finished');
+    },
+  );
+
+  streamController.close();
+}
+
 Future<void> main() async {
   await streamFromIterable();
   await reverseCountdown();
+  streamController();
 }
